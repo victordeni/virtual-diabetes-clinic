91b73fb87977e9191f5f0862ecb5f55448480061
cristina-lobyco <cristina.damov@lobyco.com>
2025-10-20 18:24:15 +0200

    mock model in test api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,27 +1,41 @@
+from unittest.mock import patch
 from fastapi.testclient import TestClient
+import numpy as np
 
 from app.main import app
 
 client = TestClient(app)
 
+def mock_load_model(*args):
+    class MockModel:
+        def predict(self, X):
+            return np.array([235.94])
+    return MockModel(), {"version": "v0.2"}
+
+@patch('app.main.load_model', mock_load_model)
 def test_health():
     r = client.get("/health")
     assert r.status_code == 200
     assert r.json()["status"] == "ok"
+    assert r.json()["model_version"] == "v0.2"
 
+@patch('app.main.load_model', mock_load_model)
 def test_predict_ok():
     payload = {
-        "age":0.02,
-        "sex":-0.044,
-        "bmi":0.06,
-        "bp":-0.03,
-        "s1":-0.02,
-        "s2":0.03,
-        "s3":-0.02,
-        "s4":0.02,
-        "s5":0.02,
-        "s6":-0.001
+        "age": 0.02,
+        "sex": -0.044,
+        "bmi": 0.06,
+        "bp": -0.03,
+        "s1": -0.02,
+        "s2": 0.03,
+        "s3": -0.02,
+        "s4": 0.02,
+        "s5": 0.02,
+        "s6": -0.001
     }
     r = client.post("/predict", json=payload)
     assert r.status_code == 200
-    assert "prediction" in r.json()
+    response = r.json()
+    assert "prediction" in response
+    assert "model_version" in response
+    assert response["model_version"] == "v0.2"
